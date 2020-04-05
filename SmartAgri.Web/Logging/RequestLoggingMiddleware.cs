8b9e1645c849c33d9ff119cb2b74547dc2065155
kreca95 <kresimir095@gmail.com>
2020-04-05 19:45:44 +0100

    added stopwatch to requestloggingmiddleware

--- SmartAgri.Web/Logging/RequestLoggingMiddleware.cs
+++ SmartAgri.Web/Logging/RequestLoggingMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace SmartAgri.Web.Logging
@@ -9,27 +10,32 @@ namespace SmartAgri.Web.Logging
     {
         private readonly RequestDelegate _next;
         private readonly ILogger _logger;
-
+        private readonly Stopwatch _stopwatch;
         public RequestLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
             _logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
+            _stopwatch = new Stopwatch();
         }
 
         public async Task Invoke(HttpContext context)
         {
             try
             {
+                _stopwatch.Start();
                 await _next(context);
             }
             finally
             {
+                _stopwatch.Stop();
                 _logger.LogInformation(
-                    "{Date} Request {method} {url} => {statusCode}",
+                    "{Date} Request {method} {url} => {statusCode} Response time: {time} ms",
                     DateTime.Now,
                     context.Request?.Method,
                     context.Request?.Path.Value,
-                    context.Response?.StatusCode);
+                    context.Response?.StatusCode
+                    ,_stopwatch.ElapsedMilliseconds);
+                _stopwatch.Reset();
             }
         }
     }
